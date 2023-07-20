b29403a0c3427f17e02515d7cb214a709c209a1f
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2023-07-20 14:18:54 -0300

    Add files via upload

---
+++ Condicional_while.js
@@ -0,0 +1,7 @@
+/* Definicao das variaveis */
+var js_contador = 1;
+/* Laço de Repetição Condicional - while */
+while (js_contador <= 20) {
+    document.write('<p>' + js_contador + '&ordf; Repetição de uma única linha de comando dentro de um laço condicional: ');
+ js_contador += 1;
+}
